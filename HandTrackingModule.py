af2417f3205f4fd6732bbcc270ba4b34395bf176
Luciano Freitas <lucianoguns123@gmail.com>
2023-10-05 20:14:33 -0300

    iniciando projeto
    colocando a camera pra funcionar

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -16,7 +16,7 @@ class handDetector():
         self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
-        
+
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands,
         self.detectionCon, self.trackCon)
@@ -26,7 +26,6 @@ class handDetector():
 def findHands(self, img, draw=True):
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     self.results = self.hands.process(imgRGB)
-    # print(results.multi_hand_landmarks)
 
     if self.results.multi_hand_landmarks:
         for handLms in self.results.multi_hand_landmarks:
@@ -42,12 +41,10 @@ def findPosition(self, img, handNo=0, draw=True):
     if self.results.multi_hand_landmarks:
         myHand = self.results.multi_hand_landmarks[handNo]
     for id, lm in enumerate(myHand.landmark):
-        # print(id, lm)
         h, w, c = img.shape
         cx, cy = int(lm.x * w), int(lm.y * h)
         xList.append(cx)
         yList.append(cy)
-        # print(id, cx, cy)
         self.lmList.append([id, cx, cy])
         if draw:
             cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
